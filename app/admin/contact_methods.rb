75c34a72595cd77aad3a9f0f2bedb71c3b05378a
Raghu Betina <raghu@firstdraft.com>
2019-05-22 00:31:08 +0000

    Whitelist columns in ContactMethod for ActiveAdmin

--- app/admin/contact_methods.rb
+++ app/admin/contact_methods.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register ContactMethod do
+
+ permit_params :method_name
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
