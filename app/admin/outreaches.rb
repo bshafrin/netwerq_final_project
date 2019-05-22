f375c064952220afaa5af3e73a8deb5cc286647b
Raghu Betina <raghu@firstdraft.com>
2019-05-22 00:31:04 +0000

    Whitelist columns in Outreach for ActiveAdmin

--- app/admin/outreaches.rb
+++ app/admin/outreaches.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Outreach do
+
+ permit_params :recipient_id, :notes, :response_tf, :outreach_method_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
